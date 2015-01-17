57ebf1f01cbe8b80b6349923d6ddda68f28ee03f
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2015-01-17 17:46:43 +0700

    Delete refactor to tripoin-root-gwt

--- root-gwt/gwt-server/src/main/java/com/tripoin/gwt/server/SampleAppServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package com.tripoin.gwt.server;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface SampleAppServiceAsync {
-    void getMessage(String msg, AsyncCallback<String> async);
-}
