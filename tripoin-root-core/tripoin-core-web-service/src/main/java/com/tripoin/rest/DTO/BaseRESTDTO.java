b48f1ae3b4e5cf30ab6a9dab7170e7b83fd60c27
KNIGHTMASTER <KNIGHTMASTER@users.noreply.github.com>
2015-02-14 16:31:29 +0700

    Post and Get Rest

---
+++ tripoin-root-core/tripoin-core-web-service/src/main/java/com/tripoin/rest/DTO/BaseRESTDTO.java
@@ -0,0 +1,7 @@
+package com.tripoin.rest.DTO;
+
+import java.io.Serializable;
+
+public class BaseRESTDTO extends ABaseRESTDTO implements Serializable{
+
+}
---
+++ tripoin-root-core/tripoin-core-web-service/src/main/java/com/tripoin/rest/controller/api/IBaseRESTController.java
@@ -0,0 +1,8 @@
+package com.tripoin.rest.controller.api;
+
+public interface IBaseRESTController {
+
+	public void writeLog();
+	
+	public String testRest();
+}
